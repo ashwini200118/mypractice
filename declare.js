6cfad4dd071f83c167fe55404a89d8d0c8159fce
ashwini200118 <ashwinitoranagatti2001@gmail.com>
2025-10-28 12:08:31 +0530

    add files

---
+++ declare.js
@@ -0,0 +1,8 @@
+// declare.js
+function showVariables() {
+    var name = "Ashwini";      // var - function scoped
+    let age = 23;              // let - block scoped
+    const city = "Mysuru";     // const - constant value
+
+    console.log("Name: " + name + "\nAge: " + age + "\nCity: " + city);
+}
